8c04ebc782a63158c99fbb28533b56f753e91e6e
cmkid <cmkid@hanmail.net>
2018-03-19 10:28:16 +0900

    Update label_image01.py
    threading test

--- label_image01.py
+++ label_image01.py
@@ -24,10 +24,10 @@ import time
 import model
 import numpy as np
 import tensorflow as tf
+import threading
 
-if __name__ == "__main__":
-  model_file = "/home/health/retrained_graph.pb"
-  label_file = "/home/health/retrained_labels.txt"
+
+def run_inference(graph, labels, file_name):
   input_height = 299
   input_width = 299
   input_mean = 0
@@ -35,15 +35,6 @@ if __name__ == "__main__":
   input_layer = "Mul"
   output_layer = "final_result"
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--image", help="image to be processed")
-  args = parser.parse_args()
-
-  if args.image:
-    file_name = args.image
-
-  start = time.time()
-  graph = model.load_graph(model_file)
   t = model.read_tensor_from_image_file(file_name,
                                   input_height=input_height,
                                   input_width=input_width,
@@ -56,17 +47,14 @@ if __name__ == "__main__":
   output_operation = graph.get_operation_by_name(output_name);
 
   with tf.Session(graph=graph) as sess:
-    start = time.time()
+    # start = time.time()
     results = sess.run(output_operation.outputs[0],
                       {input_operation.outputs[0]: t})
-    end=time.time()
+    # end=time.time()
   results = np.squeeze(results)
-
   top_k = results.argsort()[-5:][::-1]
-  labels = model.load_labels(label_file)
 
-  end=time.time()
-  print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+  # print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
 
   d = ""
   print_str = ""
@@ -74,3 +62,21 @@ if __name__ == "__main__":
     print_str += "{}{}++{:.3f}".format(d, labels[i], results[i])
     d = "||"
   print(print_str)
+
+if __name__ == "__main__":
+  model_file = "/home/health/retrained_graph.pb"
+  label_file = "/home/health/retrained_labels.txt"
+
+  graph = model.load_graph(model_file)
+  labels = model.load_labels(label_file)
+
+  while True:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", help="image to be processed")
+    args = parser.parse_args()
+
+    if args.image:
+      file_name = args.image
+    
+    t = threading.Thread(target=run_inference, args=(graph, labels, file_name,))
+    t.start()
